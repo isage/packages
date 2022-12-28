ee2d3a502a00b1c2606e7796a5b451d611bb9601
Ivan Epifanov <isage.dna@gmail.com>
2022-12-28 20:29:43 +0300

    Meh

--- upload.sh
+++ upload.sh
@@ -10,7 +10,7 @@ if [ "${GITHUB_REF##*/}" = "master" ]; then
 	if [ -n "$FILE" ]; then
 		source VITABUILD
 		echo "Uploading $FILE as $pkgname.tar.xz..."
-		sshpass -e sftp travis@dl.vitasdk.org:pkg <<< $"put $FILE $pkgname.tar.xz"
+		#sshpass -e sftp travis@dl.vitasdk.org:pkg <<< $"put $FILE $pkgname.tar.xz"
 	fi
 	cd ..
 fi
